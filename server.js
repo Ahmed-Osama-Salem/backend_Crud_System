aa9b07edec52a2324f6d96b31ac523a4cee2d93d
Ahmed-Osama-Salem <97908950+Ahmed-Osama-Salem@users.noreply.github.com>
2022-10-15 20:27:25 +0200

    updatedata

--- server.js
+++ server.js
@@ -70,10 +70,29 @@ app.delete("/delete/:mongoId", async (req, res) => {
 app.put("/update/:mongoId", async (req, res) => {
   const newConstrDate = req.body.newConstrDate;
   const mongoId = req.params.mongoId;
-
+  const dateNow = req.body.dateNow;
+  const rkmElw7da = req.body.rkmElw7da;
+  const elbnd = req.body.elbnd;
+  const techNumber = req.body.techNumber;
+  const mosadNumber = req.body.mosadNumber;
+  const noteAdd = req.body.noteAdd;
+  const kmiatMon = req.body.kmiatMon;
+  const tnfizState = req.body.tnfizState;
+  const angaz = req.body.angaz;
+  const notes = req.body.notes;
   try {
     await TableModel.findByIdAndUpdate(mongoId, {
       "allText.twqi3": newConstrDate,
+      "allText.dateNow": dateNow,
+      "allText.rkmElw7da": rkmElw7da,
+      "allText.elbnd": elbnd,
+      "allText.techNumber": techNumber,
+      "allText.mosadNumber": mosadNumber,
+      "allText.noteAdd": noteAdd,
+      "allText.kmiatMon": kmiatMon,
+      "allText.tnfizState": tnfizState,
+      "allText.angaz": angaz,
+      "allText.notes": notes,
     });
     res.send("done");
   } catch (err) {
